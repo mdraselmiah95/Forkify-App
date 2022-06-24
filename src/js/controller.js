fde79dae2bdc0609a7ae7d92e34878d1b895c067
mdraselmiah95 <russelroy15@gmail.com>
2022-06-24 23:09:21 +0600

    WIP on Forkify App

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,30 @@
-const recipeContainer = document.querySelector(".recipe");
+import * as model from "./model";
+import RecipeView from "./views/recipeView";
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import { async } from "regenerator-runtime";
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2) Loading recipe
+    await model.loadRecipe(id);
+
+    // 3) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.error(err);
+  }
 };
